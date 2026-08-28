d98299d2667a2f54f5c8270823631a15f009d103
MarianaAlvesDantasDosSantos <claricealvessantos82@gmail.com>
2026-08-28 11:37:08 -0300

    Continuação

--- GMusic/screens/MusicPlayer.js
+++ GMusic/screens/MusicPlayer.js
@@ -16,14 +16,51 @@ export default function MusicPlayer() {
     const { width } = useWindowDimensions();
     const [selectedIndex, setSelectedIndex] = useState();
     
+    const currentSong = songs[selectedIndex];
+    const artworkSize = Math.min(width-40, 380);
+
+    function handleMomentEnd(event) {
+        const offset = event.nativeEvent.contentOffset.x;
+        const index = Math.round(offset / width);
+        setSelectedIndex(index);
+    }
+
+    function renderArtwork({ item }) {
+        return (
+            <View style={[styles.artworkPage, { width }]}>
+                <Image
+                source={ item.artwork }
+                style={[
+                    styles.artworkSize, 
+                    { height: artworkSize, width: artworkSize }
+                ]}
+            />
+            </View>
+        );
+    }
+
   return (
     <SafeAreaView> style={styles.container}
-    <View styles={styles.content}>
+    <View styles={styles.header}>
         <Text style={styles.eyebrow}> TOCANDO AGORA </Text>
-         <Text style={styles.title}> GMusic </Text>
-         <Text style={styles.description}>
-            Nosso player começa aqui.
-             </Text>
+         <Text style={styles.counter}> 
+            {selectedIndex + 1} de {songs.length} 
+            </Text>
+    </View>
+
+    <FlatList
+      data={songs}
+      horizontal
+      paddingEnabled
+      renderItem={renderArtwork}
+      keyExtractor={(item) => String(item.id)}
+      showsHorizontalScrollIdicator={false}
+      onMomentumScrollEnd={handleMomentEnd}
+    />
+    
+    <View style={styles.metadata}>
+        <Text style={styles.songTitle}>{currentSong.title}</Text>
+        <Text style={styles.songArtist}>{currentSong.artist}</Text>
     </View>
     </SafeAreaView>
   )
@@ -40,6 +77,13 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         padding: 24,
     },
+    header: {
+        height: 70,
+        paddingHorizontal: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-betweet',
+    },
     eyebrow:{
         color: colors.primary,
         fontSize: 11,
@@ -52,7 +96,35 @@ const styles = StyleSheet.create({
         foteSize: 32,
         fontWeight: '800',
     },
-     description: {
+    counter: {
+        color: colors.textSecondary,
+        fontSize: 12,
+    },
+    artworkPage:{
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    artwork: {
+        borderRadius: 24,
+    },
+    metadata:{
+        minHeight: 110,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingHorizontal: 24,
+    },
+    songsTitle: {
+        color: colors.text,
+        fontSize: 22,
+        fontWeight: '800',
+        textAlign: 'center',
+    },
+    songArtist: {
+        marginTop: 6,
+        color: colors.textSecondary,
+        fontSize: 14,
+    },
+    description: {
         marginTop: 10,
         color: colors.textSecondary,
         foteSize: 15,
